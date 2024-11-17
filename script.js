c821c31a9636a87478cb94c6b0f8341ec73fed95
cl0wnfortalk <160296915+cl0wnfortalk@users.noreply.github.com>
2024-11-17 13:56:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-window.onload = function(){
-    const HIDDEN_INPUT = document.getElementById("ipaddress");
-    const HIDDEN_FORM = document.getElementById("form_contact");
-    function sendmail(){
-        let parms = {
-            ipaddress : HIDDEN_INPUT.value
-        }
-        emailjs.send("service_hx1i7vu", "template_9g41ryz", parms).then("sent");
-    }
-    // Base + Confidence Area
-    let apiKey = 'a68cf2f6a505458db6e6b7d6876df751';
-    $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
-      const HIDDEN_DATA = JSON.stringify(data, null, 2);
-      HIDDEN_INPUT.value = HIDDEN_DATA;
-      sendmail();
-    });
-}
+// window.onload = function(){
+//     const HIDDEN_INPUT = document.getElementById("ipaddress");
+//     const HIDDEN_FORM = document.getElementById("form_contact");
+//     function sendmail(){
+//         let parms = {
+//             ipaddress : HIDDEN_INPUT.value
+//         }
+//         emailjs.send("service_hx1i7vu", "template_9g41ryz", parms).then("sent");
+//     }
+//     // Base + Confidence Area
+//     let apiKey = 'a68cf2f6a505458db6e6b7d6876df751';
+//     $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+//       const HIDDEN_DATA = JSON.stringify(data, null, 2);
+//       HIDDEN_INPUT.value = HIDDEN_DATA;
+//       sendmail();
+//     });
+// }
